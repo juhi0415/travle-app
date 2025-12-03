737d854bc70a090dd4952a352b99ee232624de07
juhi0415 <juhi0415@gmail.com>
2025-12-03 11:16:47 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,38 @@
 const CACHE_NAME = 'travel-app-cache-v1';
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/app.js',
-    '/style.css',
-    '/manifest.json'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/service-worker.js',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', evt => {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('앱 쉘 캐싱 완료');
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-    evt.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.map(key => {
-                    if(key !== CACHE_NAME){
-                        console.log('이전 캐시 삭제', key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', evt => {
-    evt.respondWith(
-        caches.match(evt.request).then(resp => {
-            return resp || fetch(evt.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
